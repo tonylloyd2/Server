1830bc8456d0db35432065424431ed4ae2f767b9
lloyd  katila <101291544+tonylloyd2@users.noreply.github.com>
2023-07-17 18:41:48 +0300

    conn

---
+++ html/conn.php
@@ -0,0 +1,16 @@
+<?php
+// // Database configuration variables
+// $host = "localhost"; // Replace with your database host
+// $username = "root"; // Replace with your database username
+// $password = "1234"; // Replace with your database password
+// $database = "server"; // Replace with your database name
+
+// // Establishing the database connection
+// $conn = new mysqli($host, $username, $password, $database);
+
+// // Check the connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } else {
+echo "faled";
+// }
